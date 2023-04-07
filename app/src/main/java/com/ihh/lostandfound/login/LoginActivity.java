1706f5a8aae5a06bd1eeffea8cace791e8f4069c
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-04-07 17:41:54 +0900

    firebase로 로그인 구현

--- app/src/main/java/com/ihh/lostandfound/login/LoginActivity.java
+++ app/src/main/java/com/ihh/lostandfound/login/LoginActivity.java
@@ -1,16 +1,71 @@
 package com.ihh.lostandfound.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.ihh.lostandfound.MainActivity;
 import com.ihh.lostandfound.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText mEmail;
+    EditText mPassword;
+    Button login;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mEmail = findViewById(R.id.emailArea);
+        mPassword = findViewById(R.id.passwordArea);
+        login = findViewById(R.id.loginBtn);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        setOnLoginButton();
+    }
+
+    private void setOnLoginButton() {
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    //back stack의 데이터 날리기(뒤로가기시 회원가입으로 돌아오지 않게)
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "로그인 실패패", Toast.LENGTH_SHORT).show();
+
+
+                               }
+                            }
+                        });
+            }
+        });
+
+
     }
 }

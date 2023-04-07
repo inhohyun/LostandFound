6153aedee17ebd76a80827394e10273e5a3b7c9d
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-04-07 22:32:28 +0900

    바텀네비게이션 구현

--- app/src/main/java/com/ihh/lostandfound/MainActivity.java
+++ app/src/main/java/com/ihh/lostandfound/MainActivity.java
@@ -1,14 +1,60 @@
 package com.ihh.lostandfound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ihh.lostandfound.fragment.FindFragment;
+import com.ihh.lostandfound.fragment.LostFragment;
+import com.ihh.lostandfound.fragment.MyFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //시작을 lostFragment로
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new LostFragment()).commit();
+
+        fragmentSetting();
+    }
+
+    private void fragmentSetting() {
+
+         BottomNavigationView.OnNavigationItemSelectedListener navListener =
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+
+                        switch (item.getItemId()) {
+                            case R.id.navigation_lost:
+                                selectedFragment = new LostFragment();
+                                break;
+                            case R.id.navigation_find:
+                                selectedFragment = new FindFragment();
+                                break;
+                            case R.id.navigation_my:
+                                selectedFragment = new MyFragment();
+                                break;
+                        }
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                selectedFragment).commit();
+
+                        return true;
+
+                    }
+                };
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
     }
 }
---
+++ app/src/main/java/com/ihh/lostandfound/fragment/LostFragment.java
@@ -0,0 +1,33 @@
+package com.ihh.lostandfound.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ihh.lostandfound.R;
+
+
+public class LostFragment extends Fragment {
+
+
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_lost, container, false);
+    }
+}
